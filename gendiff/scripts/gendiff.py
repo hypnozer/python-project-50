d6deed707b05795908db436a5c7c901d77935eba
vladimirshkarin <vladimir.shkarin@gmail.com>
2024-08-23 03:37:52 +0300

    Implementation of help output

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,5 +1,16 @@
+import argparse
+
+
 def main():
-    print("")
+    parser = argparse.ArgumentParser(
+        prog="gendiff",
+        description="Compares two configuration files and shows a difference.",
+        usage="%(prog)s [-h] first_file second_file"
+    )
+    parser.add_argument("first_file")
+    parser.add_argument("second_file")
+
+    parser.print_help()
 
 
 if __name__ == "__main__":
