c30c3051ef50bebd7df3c33cde548612ec3c860d
vladimirshkarin <vladimir.shkarin@gmail.com>
2024-10-20 11:21:57 +0300

    Update gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -5,7 +5,6 @@ def main():
     parser = argparse.ArgumentParser(
         prog="gendiff",
         description="Compares two configuration files and shows a difference.",
-        usage="%(prog)s [-h] first_file second_file"
     )
     parser.add_argument("first_file")
     parser.add_argument("second_file")
